c5e3be919281c8a36cec0045745e79e7c24cfff7
Boyor Annie Thérèse BADIANE <boyorthrse@gmail.com>
2023-10-19 12:20:06 +0000

    version finale

---
+++ connexion.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style-conn.css">
+   
+    <title>E-Taxibokko</title>
+    
+</head>
+<body>
+<form action="traitement.php" method="POST">
+
+    <h2>Inscription</h2>
+<legend><span class="text"></span></legend>
+<p>Votre chauffeur en un clic!</p>
+<button type="submit">Continuez avec Facebook</button><br><br>
+ <div class="line"> 
+    <div class="gauche"></div>
+ <span>Ou</span>
+ <div class="droite"></div>
+ </div>
+ 
+Email<br>
+<input type="text" name="email" placeholder=""><br><br>
+
+
+MOT DE PASSE<br>
+<div class = "input">
+<input type="password" name="motpass" >
+
+</div> <br><br>
+
+<label>J'ai déjà un compte</label>   <label style="padding-left: 100px;"></label> <button type="submit" name="connexion">Se Connecter 🠒</button>
+
+
+
+
+
+    
+</form>
+
+</body>
+</html>
